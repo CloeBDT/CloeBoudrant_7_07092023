8cb1c439043aeed4bd4f66b9dbe27aafcbce6833
Cloé Boudrant <cloeboudrant@protonmail.com>
2023-10-05 10:52:20 +0200

    minor fix

--- js/recipeSearcher.js
+++ js/recipeSearcher.js
@@ -1,11 +1,9 @@
 import { recipes } from "../data/recipes";
 
 export function recipeSearcher (letters) {
-    let result;
     if(letters.length > 2 || letters.length === 0) {
-        result = recipes.filter((unit) => JSON.stringify(unit).toLowerCase().includes(letters.toLowerCase()))
+       return recipes.filter((unit) => JSON.stringify(unit).toLowerCase().includes(letters.toLowerCase()))
     }
-    return result;
 };
 
 
